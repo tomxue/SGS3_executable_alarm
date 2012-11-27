795dfc3e040dbf18f6bb0b9ba54829d7b200f09d
Tom Xue <tomxue0126@gmail.com>
2012-11-27 17:54:39 +0800

    README updated: how to make executable on SGS3 phone

---
+++ ex.c
@@ -0,0 +1,12 @@
+// will count untill 1s
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int counter;
+    alarm(1);
+    for(counter=0; 1; counter++)
+        printf("counter=%d ", counter);
+    return 0;
+}
